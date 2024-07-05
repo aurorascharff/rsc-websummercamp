a9b47905c5ccc3e73a302bd1839b786d5d199bcd
Aurora Scharff <aurora.sofie@gmail.com>
2024-07-05 10:59:49 +0200

    Update

--- lib/services/getContact.ts
+++ lib/services/getContact.ts
@@ -1,10 +1,7 @@
 import { notFound } from 'next/navigation';
 import { prisma } from '@/db';
-import { slow } from '@/utils/slow';
 
 export async function getContact(contactId: string) {
-  await slow();
-
   const contact = await prisma.contact.findUnique({
     where: {
       id: contactId,
--- lib/services/getContacts.ts
+++ lib/services/getContacts.ts
@@ -1,10 +1,7 @@
 import 'server-only';
 
 import { prisma } from '@/db';
-import { slow } from '@/utils/slow';
 
 export async function getContacts() {
-  await slow();
-
   return prisma.contact.findMany({ orderBy: [{ first: 'asc' }, { last: 'asc' }] });
 }
